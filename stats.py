d732d57abf9c767183aff15256580b0da861b5cf
aggrimm <agrimmps@gmail.com>
2025-07-07 09:45:36 -0400

    Bookbot with report function

--- stats.py
+++ stats.py
@@ -1,14 +1,26 @@
-def wordct(text):
+def get_num_words(text):
     words = text.split()
-    nwords = len(words)
-    return nwords
+    return len(words)
 
-def charct(text):
-    text = text.lower()
-    chardict = {}
-    for char in text:
-        if char in chardict:
-            chardict[char] += 1
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            chardict[char] = 1
-    return chardict
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
